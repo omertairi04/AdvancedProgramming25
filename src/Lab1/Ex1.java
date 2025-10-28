fcee817e27d2f3272426e830f0df9432e249a262
Omer Tairi <tairiomer04@gmail.com>
2025-10-28 20:40:12 +0100

    ssss

--- src/Lab1/Ex1.java
+++ src/Lab1/Ex1.java
@@ -9,7 +9,6 @@ class Bank {
 
     public double totalTransfers = 0;
     public double totalProvisions = 0;
-
     Bank(String name, Account[] accounts) {
         this.name = name;
         this.accounts = accounts;
